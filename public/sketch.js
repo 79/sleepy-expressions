b4f79111a13b5b655bf71751c8db8c2ec481f006
kkker <stefonia@hotmail.com>
2018-02-21 00:45:16 -0500

    kai fix the sound

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,9 @@
 // Open and connect socket
-let socket = io();
+var socket = io();
 // Keep track of users
-let users = {};
+var users = {};
+// keep track of osc objects
+var osc = {};
 
 
 // The reason why I changed this part is because if we use a total random RGB, 
@@ -27,11 +29,14 @@ function stringToColor(str) {
 
 // Create new user
 function createNewUser(id) {
-  users[id] = {
+  users[id] = { 
     username: '',
     pressed: [],
     color: stringToColor(id)
   }
+  //create a independent osc as create new user
+  setTone(id);
+  console.log(osc);
 }
 
 function setup() {
@@ -67,7 +72,6 @@ function setup() {
     if (!(id in users)) {
       createNewUser(id);
     }
-
    
     users[id].pressed[message.keycode] = {
       startTime: message.startTime,
@@ -75,7 +79,11 @@ function setup() {
       xpos: message.xpos,
       ypos: message.ypos
     };
-
+    //play the last pressed tone with corrospond osc
+    let pressedKeys = users[id].pressed;
+    //playTone(id, pressedKeys[pressedKeys.length]);
+    playTone(id, random(20,1000));
+    //playTone(id, pressedKeys[pressedKeys.length]);
   });
 
   // Receive message from server
@@ -90,14 +98,16 @@ function setup() {
     if (message.keycode in users[id].pressed) {
       users[id].pressed[message.keycode].endTime = message.endTime;
     }
+    //stop tone as key up
+    stopTone(id);
   });
 
   // Remove disconnected users
   socket.on('disconnected', function(id){
     delete users[id];
+    //delete osc object
+    osc.splice(id, 1)
   });
-  //setup osc object
-  setTone();
 }
 
 let isPressed = [];
@@ -135,12 +145,12 @@ function draw() {
   background(255);
   // noStroke();
   
-  var freq = 0;
+  //var freq = 0;
 
   for (let id in users) {
     let user = users[id];
     let username = user.username;
-    freq = freq+user.keycode*5;
+    //freq = freq+user.keycode*5;
 
     fill(user.color);
 
@@ -163,8 +173,9 @@ function draw() {
         // console.log(keychar, size, keyinfo);
       }
     }
+    //when to trigger playTone
+    //playTone(id,freq);
   }
-  playTone(freq);
 }
 
 // Send username as it changes
@@ -176,15 +187,20 @@ function usernameChanged() {
 
 
 //Make noise as the key pressed
-function setTone(){
-  osc = new p5.Oscillator(); 
-  osc.setType('sine'); 
-  osc.amp(0);
-  osc.start();
+function setTone(id){
+  osc[id] = new p5.Oscillator();
+  osc[id].setType('sine'); 
+  osc[id].amp(0);
+  osc[id].start();
 }
 
-function playTone(toneFreq){
-  osc.freq(toneFreq);
-  osc.amp(0.5,0.05); 
+//which key trigger which tone
+function playTone(oscId,tone){
+  osc[oscId].freq(tone);
+  osc[oscId].amp(0.5,0.05); 
 }
-  
+
+//stop sound
+function stopTone(oscId){
+  osc[oscId].amp(0);
+} 
